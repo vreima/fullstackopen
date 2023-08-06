b1bf67b3e6eaad5467089d7f670244be6fd0db22
Ville <ville.reima@gmail.com>
2023-08-06 11:38:35 +0300

    Ex 1.5

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,48 +1,51 @@
 const Header = (props) => {
-  return <><h1>{props.course}</h1></>
+  return <><h1>{props.course.name}</h1></>
 };
 
 const Part = (props) => {
   return <p>
-    {props.part} {props.exercises}
+    {props.part.name} {props.part.exercises}
   </p>
 };
 
 const Content = (props) => {
+  console.log(props);
   return <>
-    <Part part={props.part1} exercises={props.exercises1} />
-    <Part part={props.part2} exercises={props.exercises2} />
-    <Part part={props.part3} exercises={props.exercises3} />
+    <Part part={props.course.parts[0]} />
+    <Part part={props.course.parts[1]} />
+    <Part part={props.course.parts[2]} />
   </>
 };
 
 const Total = (props) => {
-  return <p>Number of exercises {props.total}</p>
+  return <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
 };
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1.name} exercises1={part1.exercises}
-        part2={part2.name} exercises2={part2.exercises}
-        part3={part3.name} exercises3={part3.exercises} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
